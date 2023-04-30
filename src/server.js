22bd7bb6add4cefe34679f1408e7f3ea3affa92c
raassh-23 <muh.nur.abdurrauf@gmail.com>
2023-04-30 22:11:20 +0700

    add simple hapi server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,36 @@
-console.log('halo');
+require('dotenv').config();
+
+const {name, version} = require('../package.json');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return {
+        error: false,
+        message: 'API is running',
+        data: {
+          name,
+          version,
+        },
+      };
+    },
+  });
+
+  await server.start();
+  console.log(`Server running at ${server.info.uri}`);
+};
+
+init();
