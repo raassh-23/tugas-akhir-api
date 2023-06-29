86e5b5c2200256212c47a4dfc544aaaec77f5020
raassh-23 <muh.nur.abdurrauf@gmail.com>
2023-06-29 13:06:50 +0700

    change validation

--- src/validator/leaderboard/schema.js
+++ src/validator/leaderboard/schema.js
@@ -3,18 +3,18 @@ const Joi = require('joi');
 const LeaderboardPayloadSchema = Joi.object({
   level: Joi.number().integer().min(0).required(),
   username: Joi.string().max(100).required(),
-  steps: Joi.number().integer().min(0).required(),
-  codeBlocks: Joi.number().integer().min(0).required(),
-  timeMs: Joi.number().integer().min(0).required(),
+  steps: Joi.number().integer().min(1).required(),
+  codeBlocks: Joi.number().integer().min(1).required(),
+  timeMs: Joi.number().integer().min(1).required(),
 });
 
 const LeaderboardQuerySchema = Joi.object({
   level: Joi.number().integer().min(0).required(),
   sortBy: Joi.string().valid('steps', 'codeBlocks', 'timeMs')
-      .default('steps'),
-  order: Joi.string().valid('asc', 'desc').default('desc'),
+      .default('timeMs'),
+  order: Joi.string().valid('asc', 'desc').default('asc'),
   page: Joi.number().integer().min(1).default(1),
-  pageSize: Joi.number().integer().min(1).default(10),
+  pageSize: Joi.number().integer().min(1).default(5),
 });
 
 module.exports = {
